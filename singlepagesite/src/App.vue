068708e82f37ae1823c08a70b3f03d9561296870
Gary Walls <gary.a.walls@gmail.com>
2020-11-04 15:49:54 +0000

    adds global style sheet

--- singlepagesite/src/App.vue
+++ singlepagesite/src/App.vue
@@ -9,12 +9,13 @@
 </template>
 
 <style lang="scss">
+@import "src/assets/scss/main.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $primary;
 }
 
 #nav {
